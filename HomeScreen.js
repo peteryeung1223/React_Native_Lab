9523ce73cc0399ea8c88896da319f0afbc545e17
peteryeung1223 <peteryeung1223@gmail.com>
2025-03-21 20:27:09 +0800

    Lab2

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,23 +1,87 @@
 import React from 'react';
-   import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import data from './data.json';
 
-   const HomeScreen = () => {
-     return (
-       <View style={styles.container}>
-         <Text style={styles.title}>Welcome to the Home Screen!</Text>
-       </View>
-     );
-   };
+const HomeScreen = () => {
+  const renderItem = ({ item }) => {
+    // const items = [];
+    // for (let i = 0; i < data.length; i++) {
+    //   const item = data[i];
+    //   const date = new Date(item.createdAt);
+    //   const formattedDate = date.toLocaleDateString()
+    //   const formattedTime = date.toLocaleTimeString()
 
-   const styles = StyleSheet.create({
-     container: {
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
-     },
-     title: {
-       fontSize: 24,
-     },
-   });
+    //   items.push(
+    //     <View style={styles.itemContainer}>
+    //     <Text style={styles.title}>{item.title}</Text>
+    //     <Text style={styles.description}>{item.description}</Text>
+    //     <Text style={styles.meta}>by: {item.user} - Comments: {item.commentCount}</Text>
+    //     <Text style={styles.meta}>{formattedDate} {formattedTime}</Text>
+    //     </View>);
+    // }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Todo List (FlatList)</Text>
+      {/* {data.map((item) => { */}
+        const date = new Date(item.createdAt);
+        const formattedDate = date.toLocaleDateString()
+        const formattedTime = date.toLocaleTimeString()
+  
+        return(
+          
+
+          <View key={item._id} style={styles.itemContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+          <Text style={styles.meta}>by: {item.user} - Comments: {item.commentCount}</Text>
+          <Text style={styles.meta}>{formattedDate} {formattedTime}</Text>
+          </View>
+          );
+      {/* })} */}
+      <FlatList 
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item=item._id)}
+      />
+    </View>
+  );
+};
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+  itemContainer: {
+    padding: 10,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 14,
+    marginVertical: 5,
+  },
+  meta: {
+    fontSize: 12,
+    color: '#666',
+  },
+});
+
 
    export default HomeScreen;
